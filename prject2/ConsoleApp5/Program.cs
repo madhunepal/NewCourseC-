4eddf3afd8d226c86d1039807aad28c37e070986
madhunepal <madhunepal@gmail.com>
2022-02-28 21:14:57 -0500

    new project2 is added

---
+++ prject2/ConsoleApp5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("project 2 codes.");
+            Console.ReadLine(); 
+        }
+    }
+}
